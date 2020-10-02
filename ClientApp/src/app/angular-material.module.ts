c7b4d7a40a29820060f5b49a4e68c127915f207b
Jacob <jacobhtnguyen@gmail.com>
2020-10-02 17:37:40 -0400

    added ability to filter names of cities

--- ClientApp/src/app/angular-material.module.ts
+++ ClientApp/src/app/angular-material.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
     imports: [
         MatTableModule,
         MatPaginatorModule,
-        MatSortModule
+        MatSortModule,
+        MatInputModule
     ],
     exports: [
         MatTableModule,
         MatPaginatorModule,
-        MatSortModule
+        MatSortModule,
+        MatInputModule
     ]
 })
 
